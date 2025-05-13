aca4c0aab1710ba61f1b80327fd2fe240ab64f81
KhoiChau204 <khoichau.work@gmail.com>
2025-05-13 21:16:32 +0700

    Initial Commit

---
+++ SimpleToDo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleToDo
+//
+//  Created by Chau Anh Khoi on 13/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleToDo/SimpleToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleToDoApp.swift
+//  SimpleToDo
+//
+//  Created by Chau Anh Khoi on 13/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SimpleToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
